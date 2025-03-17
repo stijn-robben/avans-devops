bf588b7d56a0ff3d7982fe77380b05783a616f2b
Steen010 <kjh.vansteen@student.avans.nl>
2025-03-17 13:10:09 +0100

    Added backlog state pattern

---
+++ demo/src/main/java/com/example/BacklogStateLib/DoneState.java
@@ -0,0 +1,44 @@
+package com.example.BacklogStateLib;
+
+public class DoneState implements IBacklogState {
+    
+    public DoneState() {
+        
+    }
+    
+    @Override
+    public void startBacklogItem() {
+        System.out.println("Cannot start a backlog item that is already done.");
+    }
+    
+    @Override
+    public void finishImplementingItem() {
+        System.out.println("Cannot finish implementing a backlog item that is already done.");
+    }
+    
+    @Override
+    public void startTestingItem() {
+        System.out.println("Cannot start testing a backlog item that is already done.");
+    }
+    
+    @Override
+    public void finishTestingItem() {
+        System.out.println("Cannot finish testing a backlog item that is already done.");
+    }
+    
+    @Override
+    public void deployItem() {
+        System.out.println("Cannot deploy a backlog item that is already done.");
+    }
+
+    @Override
+    public void revertToTodo() {
+        System.out.println("The item is in the done state.");
+    }
+
+    @Override
+    public void revertToReadyForTesting() {
+        System.out.println("The item is in the done state.");
+    }
+
+}
